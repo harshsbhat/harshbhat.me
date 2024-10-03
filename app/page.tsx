4d06081d9b413acf14223606f2240325f744acd5
Harsh Shrikant Bhat <90265455+harshsbhat@users.noreply.github.com>
2024-10-03 11:57:56 +0530

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Harsh Bhat
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Hi there, I'm Harsh Bhat, and I'm about to finish my Bachelor's in Computer Science and Engineering. By the way, I love open source and NextJs!`}
       </p>
       <div className="my-8">
         <BlogPosts />
